f8862f0aca243770b58b131fe1bb633b64df31d5
Luis Llamas <luisllamas@hotmail.com>
2019-09-28 20:15:31 +0200

    Added lesson 15

---
+++ 15_Forms_LED_PWM/Server.hpp
@@ -0,0 +1,27 @@
+AsyncWebServer server(80);
+
+void handleFormPWM(AsyncWebServerRequest *request)
+{
+ String pwmValue = request->arg("pwmValue");
+ 
+ Serial.print("PWM:\t");
+ Serial.println(pwmValue);
+
+ analogWrite(LED_BUILTIN, 1023 - pwmValue.toInt());
+ 
+ request->redirect("/");
+}
+
+void InitServer()
+{
+	server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+
+	server.on("/LED", HTTP_POST, handleFormPWM);
+
+	server.onNotFound([](AsyncWebServerRequest *request) {
+		request->send(400, "text/plain", "Not found");
+	});
+
+	server.begin();
+    Serial.println("HTTP server started");
+}
