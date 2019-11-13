b65934049ca00f676417cbe2cf7d260e60ae738f
Luis Llamas <luisllamas@hotmail.com>
2019-11-13 18:30:38 +0100

    Added lesson 21

---
+++ 21_API_REST_Client/21_API_REST_Client.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+#include "config.h"  // Sustituir con datos de vuestra red
+#include "API.hpp"
+#include "ESP8266_Utils.hpp"
+
+void setup() 
+{
+	Serial.begin(115200);
+  
+ 	ConnectWiFi_STA();
+	
+	GetAll();
+	GetItem(1);
+	GetQuery("ABC");
+	Create("New item");
+	ReplaceById(2, "New item");
+    UpdateById(2, "New item");
+	DeleteById(5);
+}
+
+void loop() 
+{
+}
+
