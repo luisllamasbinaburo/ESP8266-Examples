a7bedb8763639981b13d2402a05bea59adbf940d
Luis Llamas <luisllamas@hotmail.com>
2020-02-03 22:18:53 +0100

    Added lesson 24

---
+++ 24_Json_Ajax/Server.hpp
@@ -0,0 +1,16 @@
+AsyncWebServer server(80);
+
+void InitServer()
+{
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+
+    server.on("/LED", HTTP_GET, getData);
+    server.on("/LED", HTTP_POST, [](AsyncWebServerRequest * request){}, NULL, setData);
+  
+    server.onNotFound([](AsyncWebServerRequest *request) {
+        request->send(400, "text/plain", "Not found");
+    });
+
+    server.begin();
+    Serial.println("HTTP server started");
+}
