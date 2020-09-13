72bf22a98567f3b4c69720573e5236f5660cba67
Luis Llamas <luisllamas@hotmail.com>
2020-09-13 23:23:08 +0200

    Added lesson 30

---
+++ 30_Vue_Axios/ESP8266_Utils.hpp
@@ -0,0 +1,36 @@
+void ConnectWiFi_STA(bool useStaticIP = false)
+{
+   Serial.println("");
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, password);
+   if(useStaticIP) WiFi.config(ip, gateway, subnet);
+   while (WiFi.status() != WL_CONNECTED) 
+   { 
+     delay(100);  
+     Serial.print('.'); 
+   }
+ 
+   Serial.println("");
+   Serial.print("Iniciado STA:\t");
+   Serial.println(ssid);
+   Serial.print("IP address:\t");
+   Serial.println(WiFi.localIP());
+}
+
+void ConnectWiFi_AP(bool useStaticIP = false)
+{ 
+   Serial.println("");
+   WiFi.mode(WIFI_AP);
+   while(!WiFi.softAP(ssid, password))
+   {
+     Serial.println(".");
+     delay(100);
+   }
+   if(useStaticIP) WiFi.softAPConfig(ip, gateway, subnet);
+
+   Serial.println("");
+   Serial.print("Iniciado AP:\t");
+   Serial.println(ssid);
+   Serial.print("IP address:\t");
+   Serial.println(WiFi.softAPIP());
+}
